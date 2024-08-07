00344f7be906fc2ce3ae108540c2c285dbd89f35
Mohamed Lemine Maouloud <maouloud.mohamedlemine@gmail.com>
2024-08-07 15:53:03 +0000

    increased request body size limit

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const redis = require("redis");
 const nodemailer = require("nodemailer");
 const fs = require("fs");
 const cors = require('cors');
-
+const bodyParser = require('body-parser');
 const app = express();
 app.use(cors());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 const port = 3000;
 app.use(express.json());
 
